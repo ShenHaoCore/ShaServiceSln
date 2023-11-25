d3e7b4a402d023e9d69281064d140fe3ea8db215
ShenHaoCore <ShenHaoCore@163.com>
2023-11-25 12:03:50 +0800

    修改名称

--- Common/Sha.Framework/Swagger/SwaggerSetup.cs
+++ Common/Sha.Framework/Swagger/SwaggerSetup.cs
@@ -13,15 +13,15 @@ namespace Sha.Framework.Swagger
         /// Swagger注册
         /// </summary>
         /// <param name="services"></param>
-        /// <param name="xmlFileNames"></param>
+        /// <param name="xmlNames"></param>
         /// <exception cref="ArgumentNullException"></exception>
-        public static void AddSwaggerSetup(this IServiceCollection services, List<string> xmlFileNames)
+        public static void AddSwaggerSetup(this IServiceCollection services, List<string> xmlNames)
         {
             if (services == null) { throw new ArgumentNullException(nameof(services)); }
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             services.AddSwaggerGen(options =>
             {
-                xmlFileNames.ForEach(name => { if (File.Exists(Path.Combine(AppContext.BaseDirectory, name))) { options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, name), true); } });
+                xmlNames.ForEach(name => { if (File.Exists(Path.Combine(AppContext.BaseDirectory, name))) { options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, name), true); } });
                 options.AddSecurityRequirement(new OpenApiSecurityRequirement { { new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }, new List<string>() } });
                 options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { Description = "JWT授权 Bearer {Token}（注意两者之间是一个空格）", Name = "Authorization", In = ParameterLocation.Header, Type = SecuritySchemeType.ApiKey });
             });
