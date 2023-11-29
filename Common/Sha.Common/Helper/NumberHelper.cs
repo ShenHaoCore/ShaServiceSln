3827c263ae30df122d8a60ddf6ab8a9f821feb14
ShenHaoCore <ShenHaoCore@163.com>
2023-11-29 17:41:16 +0800

    改名

--- Common/Sha.Common/Helper/NumberHelper.cs
+++ Common/Sha.Common/Helper/NumberHelper.cs
@@ -8,20 +8,21 @@ namespace Sha.Common.Helper
     public class NumberHelper
     {
         /// <summary>
-        /// 将指定的自然数转换为26进制字符
+        /// 将正整数转换为26进制字符
         /// </summary>
         /// <param name="number">自然数</param>
         /// <returns></returns>
-        public static string ToSystem26(int number)
+        public static string ToChar(int number)
         {
             StringBuilder str = new StringBuilder();
-            while (number > 0)
+            int num = number;
+            while (num > 0)
             {
                 int remainder = number % 26;
                 if (remainder == 0) { remainder = 26; }
                 remainder = remainder + 64;
                 str.Insert(0, (char)remainder);
-                number = (number - remainder) / 26;
+                num = (num - remainder) / 26;
             }
             return str.ToString();
         }
