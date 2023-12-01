046eff7fa4097a55c09272064296110f93df8b26
ShenHaoCore <ShenHaoCore@163.com>
2023-12-01 23:23:21 +0800

    修改

--- Common/Sha.Business/Payment/Alipay.cs
+++ Common/Sha.Business/Payment/Alipay.cs
@@ -41,15 +41,7 @@ namespace Sha.Business.Payment
         /// <returns></returns>
         public ResultModel<AppPaymentTradeModel> AppTrade(AppPaymentTradeParam paramObj)
         {
-            AlipayTradeAppPayModel trade = new AlipayTradeAppPayModel()
-            {
-                Body = paramObj.Body,
-                Subject = paramObj.Subject,
-                TotalAmount = paramObj.Amount.ToString(),
-                ProductCode = ProductCode,
-                OutTradeNo = paramObj.TradeNo,
-                TimeoutExpress = TimeoutExpress
-            };
+            AlipayTradeAppPayModel trade = new AlipayTradeAppPayModel() { Body = paramObj.Body, Subject = paramObj.Subject, TotalAmount = paramObj.Amount.ToString(), ProductCode = ProductCode, OutTradeNo = paramObj.TradeNo, TimeoutExpress = TimeoutExpress };
             AlipayTradeAppPayResponse? response = client.TradeAppPay(trade);
             if (response == null || response.IsError) { return new ResultModel<AppPaymentTradeModel>(false, FrameworkEnum.StatusCode.Fail); }
             return new ResultModel<AppPaymentTradeModel>(true, FrameworkEnum.StatusCode.Success, new AppPaymentTradeModel(response.Body));
@@ -62,15 +54,7 @@ namespace Sha.Business.Payment
         /// <returns></returns>
         public ResultModel<PagePaymentTradeModel> PageTrade(PagePaymentTradeParam paramObj)
         {
-            AlipayTradePagePayModel trade = new AlipayTradePagePayModel()
-            {
-                Body = paramObj.Body,
-                Subject = paramObj.Subject,
-                TotalAmount = paramObj.Amount.ToString(),
-                ProductCode = ProductCode,
-                OutTradeNo = paramObj.TradeNo,
-                TimeoutExpress = TimeoutExpress
-            };
+            AlipayTradePagePayModel trade = new AlipayTradePagePayModel() { Body = paramObj.Body, Subject = paramObj.Subject, TotalAmount = paramObj.Amount.ToString(), ProductCode = ProductCode, OutTradeNo = paramObj.TradeNo, TimeoutExpress = TimeoutExpress };
             BusinessEnum.RequestMethod method = BusinessEnum.RequestMethod.POST; // POST方式请求，生成FORM表单
             if (paramObj.Method.Equals(BusinessEnum.RequestMethod.GET.ToString(), StringComparison.OrdinalIgnoreCase)) { method = BusinessEnum.RequestMethod.GET; } // GET方式请求，即生成URL链接;
             AlipayTradePagePayResponse? response = client.TradePagePay(trade, method);
