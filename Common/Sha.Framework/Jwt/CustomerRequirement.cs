ab7728ca6fa5b72a2a7a53ad49e659c45e878b54
ShenHaoCore <ShenHaoCore@163.com>
2023-12-11 23:10:56 +0800

    客户登录

---
+++ Common/Sha.Framework/Jwt/CustomerRequirement.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Sha.Framework.Jwt
+{
+    /// <summary>
+    /// 客户权限要求
+    /// </summary>
+    public class CustomerRequirement : IAuthorizationRequirement
+    {
+    }
+}
---
+++ Common/Sha.Framework/Jwt/CustomerRequirementHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Sha.Framework.Redis;
+
+namespace Sha.Framework.Jwt
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CustomerRequirementHandler : AuthorizationHandler<CustomerRequirement>
+    {
+        private readonly IRedisManage redis;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="redis"></param>
+        public CustomerRequirementHandler(IRedisManage redis)
+        {
+            this.redis = redis;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="requirement"></param>
+        /// <returns></returns>
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomerRequirement requirement)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
