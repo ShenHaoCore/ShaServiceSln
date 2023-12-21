9ed5c2f90f8405da7b0b190bfb2cbf105abf303f
ShenHaoCore <ShenHaoCore@163.com>
2023-12-21 19:26:23 +0800

    修改

--- Common/Sha.Common/Helper/ExcelHelper.cs
+++ Common/Sha.Common/Helper/ExcelHelper.cs
@@ -1,4 +1,5 @@
 ﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using System.Data;
 
@@ -32,31 +33,57 @@ namespace Sha.Common.Helper
         /// </summary>
         /// <param name="stream"></param>
         /// <returns></returns>
+        private static DataTable XlsToDataTable2(Stream stream)
+        {
+            using (HSSFWorkbook workbook = new HSSFWorkbook(stream))
+            {
+                return ReadDataTableFromSheet(stream, workbook.GetSheetAt(workbook.ActiveSheetIndex));
+            }
+        }
+
+        /// <summary>
+        /// EXCEL（2003版）
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
         private static DataTable XlsToDataTable(Stream stream)
         {
             DataTable table = new DataTable();
             HSSFWorkbook workbook = new HSSFWorkbook(stream);
             HSSFSheet sheet = (HSSFSheet)workbook.GetSheetAt(workbook.ActiveSheetIndex);
-            HSSFRow headrow = (HSSFRow)sheet.GetRow(0);
-            int lastCellNum = (int)headrow.LastCellNum;
-            for (int i = (int)headrow.FirstCellNum; i < lastCellNum; i++)
+            HSSFRow headerRow = (HSSFRow)sheet.GetRow(0);
+            int lastCellNum = (int)headerRow.LastCellNum;
+            for (int i = (int)headerRow.FirstCellNum; i < lastCellNum; i++)
             {
-                DataColumn column = new DataColumn(headrow.GetCell(i).StringCellValue);
+                DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
                 table.Columns.Add(column);
             }
             int lastRowNum = sheet.LastRowNum;
-            for (int i = sheet.FirstRowNum + 1; i < sheet.LastRowNum; i++)
+            for (int i = sheet.FirstRowNum + 1; i < lastRowNum; i++)
             {
-                HSSFRow bodyrow = (HSSFRow)sheet.GetRow(i);
-                DataRow row = table.NewRow();
-                for (int j = (int)bodyrow.FirstCellNum; j < lastCellNum; j++)
+                HSSFRow dataRow = (HSSFRow)sheet.GetRow(i);
+                DataRow tableRow = table.NewRow();
+                for (int j = (int)dataRow.FirstCellNum; j < lastCellNum; j++)
                 {
-                    row[j] = bodyrow.GetCell(j);
+                    tableRow[j] = dataRow.GetCell(j);
                 }
-                table.Rows.Add(row);
+                table.Rows.Add(tableRow);
             }
             stream.Close();
             return table;
+        }
+
+        /// <summary>
+        /// EXCEL（2007版）
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        private static DataTable XlsxToDataTable2(Stream stream)
+        {
+            using (XSSFWorkbook workbook = new XSSFWorkbook(stream))
+            {
+                return ReadDataTableFromSheet(stream, workbook.GetSheetAt(workbook.ActiveSheetIndex));
+            }
         }
 
         /// <summary>
@@ -69,25 +96,61 @@ namespace Sha.Common.Helper
             DataTable table = new DataTable();
             XSSFWorkbook workbook = new XSSFWorkbook(stream);
             XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(workbook.ActiveSheetIndex);
-            XSSFRow headrow = (XSSFRow)sheet.GetRow(0);
-            int lastCellNum = (int)headrow.LastCellNum;
-            for (int i = (int)headrow.FirstCellNum; i < lastCellNum; i++)
+            XSSFRow headerRow = (XSSFRow)sheet.GetRow(0);
+            int lastCellNum = (int)headerRow.LastCellNum;
+            for (int i = (int)headerRow.FirstCellNum; i < lastCellNum; i++)
             {
-                DataColumn column = new DataColumn(headrow.GetCell(i).StringCellValue);
+                DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
                 table.Columns.Add(column);
             }
             int lastRowNum = sheet.LastRowNum;
-            for (int i = sheet.FirstRowNum + 1; i < sheet.LastRowNum; i++)
+            for (int i = sheet.FirstRowNum + 1; i < lastRowNum; i++)
             {
-                XSSFRow bodyrow = (XSSFRow)sheet.GetRow(i);
-                DataRow row = table.NewRow();
-                for (int j = (int)bodyrow.FirstCellNum; j < lastCellNum; j++)
+                XSSFRow dataRow = (XSSFRow)sheet.GetRow(i);
+                DataRow tableRow = table.NewRow();
+                for (int j = (int)dataRow.FirstCellNum; j < lastCellNum; j++)
                 {
-                    row[j] = bodyrow.GetCell(j);
+                    tableRow[j] = dataRow.GetCell(j);
                 }
-                table.Rows.Add(row);
+                table.Rows.Add(tableRow);
             }
             stream.Close();
+            return table;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="sheet"></param>
+        /// <returns></returns>
+        private static DataTable ReadDataTableFromSheet(Stream stream, ISheet sheet)
+        {
+            DataTable table = new DataTable();
+
+            IRow headerRow = sheet.GetRow(0);
+            int lastCellNum = headerRow.LastCellNum;
+
+            for (int i = headerRow.FirstCellNum; i < lastCellNum; i++)
+            {
+                DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
+                table.Columns.Add(column);
+            }
+
+            int lastRowNum = sheet.LastRowNum;
+            for (int i = sheet.FirstRowNum + 1; i < lastRowNum; i++)
+            {
+                IRow dataRow = sheet.GetRow(i);
+                DataRow tableRow = table.NewRow();
+
+                for (int j = dataRow.FirstCellNum; j < lastCellNum; j++)
+                {
+                    tableRow[j] = dataRow.GetCell(j);
+                }
+
+                table.Rows.Add(tableRow);
+            }
+
             return table;
         }
     }
