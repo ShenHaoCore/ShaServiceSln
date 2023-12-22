d5f09dd83df4b18581226bcdb52467bea5f6c645
ShenHaoCore <ShenHaoCore@163.com>
2023-12-22 22:54:20 +0800

    修改身份认证

---
+++ Identity/Sha.Identity/Config.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer4.Models;
+
+namespace Sha.Identity
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope> { new ApiScope("BaseService"), new ApiScope("UserService") };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> ApiResources => new List<ApiResource>() { 
+            new ApiResource("BaseService", "BaseService") { Scopes = { "BaseService" }, ApiSecrets = { new Secret("BaseService".Sha256()) } }, 
+            new ApiResource("UserService", "UserService") { Scopes = { "UserService" }, ApiSecrets = { new Secret("UserService".Sha256()) } } 
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> Clients => new List<Client> { 
+            new Client { 
+                ClientId = "web_client", ClientName = "AuthCenter", AllowedGrantTypes = GrantTypes.ClientCredentials, 
+                ClientSecrets = new[] { new Secret("Mamba24".Sha256()) }, AccessTokenLifetime = 3600, AllowedScopes = new List<string> { "BaseService", "UserService" }, 
+                Claims = new List<ClientClaim>() { new ClientClaim(IdentityModel.JwtClaimTypes.Role, "Admin"), new ClientClaim(IdentityModel.JwtClaimTypes.NickName, "Mamba24"), } 
+            } 
+        };
+    }
+}
--- Identity/Sha.Identity/Program.cs
+++ Identity/Sha.Identity/Program.cs
@@ -1,25 +1,15 @@
+using Sha.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var identity = builder.Services.AddIdentityServer();
+identity.AddInMemoryIdentityResources(Config.IdentityResources);
+identity.AddInMemoryApiScopes(Config.ApiScopes);
+identity.AddInMemoryApiResources(Config.ApiResources);
+identity.AddInMemoryClients(Config.Clients);
+identity.AddDeveloperSigningCredential();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseIdentityServer();
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
