fbc7044785512ed898d358ff3d2e688cdcfb150c
ShenHaoCore <ShenHaoCore@163.com>
2024-01-24 16:28:07 +0800

    修改

---
+++ Sha.BaseService.Api/Common/AutofacRegisterModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using Sha.BaseService.Bll.Common;
+using Sha.BaseService.Dal.Common;
+
+namespace Sha.BaseService.Api.Common
+{
+    /// <summary>
+    /// 自动注册模块
+    /// </summary>
+    public class AutofacRegisterModule : Autofac.Module
+    {
+        /// <summary>
+        /// 加载
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(Program).Assembly).Where(t => typeof(ControllerBase).IsAssignableFrom(t) && t != typeof(ControllerBase)).PropertiesAutowired();
+            builder.RegisterAssemblyTypes(typeof(BaseServiceBll).Assembly).Where(t => typeof(BaseServiceBll).IsAssignableFrom(t) && t != typeof(BaseServiceBll)).PropertiesAutowired();
+            builder.RegisterAssemblyTypes(typeof(BaseServiceDal).Assembly).Where(t => typeof(BaseServiceDal).IsAssignableFrom(t) && t != typeof(BaseServiceDal)).PropertiesAutowired();
+        }
+    }
+}
---
+++ Sha.BaseService.Api/Controllers/V2/AddressController.cs
@@ -0,0 +1,44 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sha.BaseService.Bll;
+using Sha.BaseService.Model.DTO;
+using Sha.BaseService.Model.Request;
+using Sha.Framework.Base;
+using Sha.Framework.Enum;
+
+namespace Sha.BaseService.Api.Controllers.V2
+{
+    /// <summary>
+    /// 地址
+    /// </summary>
+    [ApiVersion(2.0)]
+    public class AddressController : ShaBaseController
+    {
+        private readonly AddressBll bll;
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        /// <param name="logger">日志</param>
+        /// <param name="bll">业务逻辑层</param>
+        public AddressController(ILogger<AddressController> logger, AddressBll bll) : base(logger)
+        {
+            this.bll = bll;
+        }
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public BaseResponse Create([FromBody] AddressCreateRequest request)
+        {
+            logger.LogDebug($"地址新增请求【{JsonConvert.SerializeObject(request)}】");
+            AddressCreateModel param = new AddressCreateModel();
+            ResultModel<bool> result = bll.Create(param);
+            if (!result.IsSuccess) { return new BaseResponse(false, result.Code, result.Message); }
+            return new BaseResponse(true, FrameworkEnum.StatusCode.Success);
+        }
+    }
+}
