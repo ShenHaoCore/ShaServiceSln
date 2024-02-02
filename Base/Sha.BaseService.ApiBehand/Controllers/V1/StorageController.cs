7498a9d892f226a24ad1bc6106b028683e6b8f3b
ShenHaoCore <ShenHaoCore@163.com>
2024-02-02 21:50:09 +0800

    修改

--- Base/Sha.BaseService.ApiBehand/Controllers/V1/StorageController.cs
+++ Base/Sha.BaseService.ApiBehand/Controllers/V1/StorageController.cs
@@ -1,7 +1,6 @@
 ﻿using Asp.Versioning;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Sha.BaseService.Bll;
 using Sha.Business.Storage;
 using Sha.Framework.Base;
 using Sha.Framework.Enum;
@@ -10,23 +9,17 @@ using System.ComponentModel;
 namespace Sha.BaseService.ApiBehand.Controllers.V1
 {
     /// <summary>
-    /// 文件
+    /// 存储
     /// </summary>
     [ApiVersion(1.0)]
     public class StorageController : ShaBaseController
     {
-        private readonly StorageBll bll;
-
         /// <summary>
-        /// 文件
+        /// 存储
         /// </summary>
         /// <param name="logger">日志</param>
         /// <param name="mapper">映射</param>
-        /// <param name="bll">业务逻辑层</param>
-        public StorageController(ILogger<ShaBaseController> logger, IMapper mapper, StorageBll bll) : base(logger, mapper)
-        {
-            this.bll = bll;
-        }
+        public StorageController(ILogger<ShaBaseController> logger, IMapper mapper) : base(logger, mapper) { }
 
         /// <summary>
         /// 上传
